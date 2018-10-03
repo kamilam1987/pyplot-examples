2fd7b8b5c562949062334a1eb21b6db294c8bd22
kamilam1987 <kamilam1987@gmail.com>
2018-10-03 21:21:41 +0100

    Added title, labels and legend to the plot

--- 2dplot.py
+++ 2dplot.py
@@ -5,10 +5,17 @@ x = np.arange(0.0, 10.0, 0.01)
 y = 3.0 * x + 1.0
 noise = np.random.normal(0.0, 1.0, len(x))
 
-plt.plot(x, y + noise, 'r.')
+plt.plot(x, y + noise, 'r.', label = "Actual")
 # b stands for blue dots instead of line
-plt.plot(x, y, 'b-')
+plt.plot(x, y, 'b-', label = "Model")
 
+# Adds title
+plt.title("Simple plot")
+# Adds label
+plt.xlabel("Weight")
+plt.ylabel("Mass")
+# Adds legend
+plt.legend()
 # shows figures
 plt.show()
 
